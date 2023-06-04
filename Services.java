43e09bb181c246f5f93221586493d09d51b4b355
˗ˏˋShubha ´ˎ˗ <93957744+ShubhaPokharel@users.noreply.github.com>
2023-06-03 21:49:50 -0400

    Update Services.java

--- Services.java
+++ Services.java
@@ -17,16 +17,16 @@ class Services{
 	public static void main(String[] args){
 	 	Services s =  new Services();
 	 	double totalAmount= s.powerBill(4,9.453);
-	 	System.out.println("Your total amount: " + totalAmount);
+	 	System.out.println("Your total amount: " + totalAmount);          //Your total amount: 37.812
 
 	 	double totalAmount2 = s.powerBill(4224,25.21545);
-	 	System.out.println("Your total amount: " + totalAmount2);
-	 		System.out.println("**************************************");
+	 	System.out.println("Your total amount: " + totalAmount2);     // Your total amount: 106510.0608
+	 		System.out.println("**************************************");    
 
 	 	 String ageLimit = s.travel("Shubha",11);
-	 	 System.out.println("Your name and age limit: " + ageLimit);
+	 	 System.out.println("Your name and age limit: " + ageLimit);     //You are.....: old enough
 
 	 	 String ageLimit2 = s.travel("Makinley",-1);
-	 	 System.out.println("Your name and age limit: "+ ageLimit2);
+	 	 System.out.println("Your name and age limit: "+ ageLimit2);       //You are.....: young
 	}
 }
